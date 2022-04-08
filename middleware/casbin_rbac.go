4e6618e8808cb1cbadcbd16a33771ef25590347c
jonssonyan <yz808@outlook.com>
2022-04-08 11:36:30 +0800

    fix: middleware

--- middleware/casbin_rbac.go
+++ middleware/casbin_rbac.go
@@ -12,21 +12,23 @@ import (
 // casbin鉴权中间件
 func CasbinHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 获取当前用户
-		userVo := util.GetCurrentUser(c)
-		roleVo, err := dao.SelectRoleById(&userVo.RoleId)
-		if err != nil {
-			vo.Fail(err.Error(), c)
-			return
-		}
 		casbin, err := dao.Casbin()
 		if err != nil {
 			vo.Fail(err.Error(), c)
+			c.Abort()
 			return
 		}
 		// 检查账户权限
 		path := c.Request.URL.Path
 		split := strings.Split(path, "?")
+		// 获取当前用户
+		userVo := util.GetCurrentUser(c)
+		roleVo, err := dao.SelectRoleById(&userVo.RoleId)
+		if err != nil {
+			vo.Fail(err.Error(), c)
+			c.Abort()
+			return
+		}
 		has, err := casbin.Enforce(roleVo.Name, split[0], c.Request.Method)
 		if err != nil {
 			vo.Fail(err.Error(), c)
