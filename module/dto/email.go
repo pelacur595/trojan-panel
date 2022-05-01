eb41f9924139eb229df9abbf55f6bd5bee8bee2f
jonssonyan <yz808@outlook.com>
2022-05-02 01:00:21 +0800

    feat: email

--- module/dto/email.go
+++ module/dto/email.go
@@ -1,8 +1,8 @@
 package dto
 
 type SendEmailDto struct {
-	FromEmailName string `json:"fromEmailName" form:"fromEmailName" validate:"omitempty,min=0,max=32"`
-	ToEmail       string `json:"toEmail" form:"toEmail" validate:"omitempty,validateEmail"`
-	Subject       string `json:"subject" form:"subject" validate:"omitempty,min=0,max=64"`
-	Content       string `json:"content" form:"content" validate:"omitempty,min=0,max=255"`
+	FromEmailName string   `json:"fromEmailName" form:"fromEmailName" validate:"omitempty,min=0,max=32"`
+	ToEmails      []string `json:"toEmails" form:"toEmails" validate:"omitempty,validateEmail"`
+	Subject       string   `json:"subject" form:"subject" validate:"omitempty,min=0,max=64"`
+	Content       string   `json:"content" form:"content" validate:"omitempty,min=0,max=255"`
 }
--- service/email.go
+++ service/email.go
@@ -1,8 +1,8 @@
 package service
 
 import (
+	"github.com/sirupsen/logrus"
 	"gopkg.in/gomail.v2"
-	"log"
 	"time"
 	"trojan/module/constant"
 	"trojan/module/dto"
@@ -16,6 +16,7 @@ func SendEmail(sendEmailDto *dto.SendEmailDto) error {
 	}
 	ch := make(chan *gomail.Message)
 	d := gomail.NewDialer(systemVo.EmailHost, int(systemVo.EmailPort), systemVo.EmailUsername, systemVo.EmailPassword)
+
 	go func() {
 		var s gomail.SendCloser
 		var err error
@@ -28,29 +29,37 @@ func SendEmail(sendEmailDto *dto.SendEmailDto) error {
 				}
 				if !open {
 					if s, err = d.Dial(); err != nil {
-						panic(err)
+						logrus.Errorf("mail dail err: %v\n", err)
+						return
 					}
 					open = true
 				}
+
 				if err := gomail.Send(s, m); err != nil {
-					log.Print(err)
+					logrus.Errorf("mail send err: %v\n", err)
+					return
 				}
+
 			// 30秒没有发送消息则关闭SMTP server连接
 			case <-time.After(30 * time.Second):
 				if open {
 					if err := s.Close(); err != nil {
-						panic(err)
+						logrus.Errorf("mail close err: %v\n", err)
+						return
 					}
 					open = false
 				}
 			}
 		}
 	}()
+
 	// 发送消息
 	m := gomail.NewMessage()
-	m.SetAddressHeader("From", systemVo.EmailUsername, sendEmailDto.FromEmailName)
-	m.SetHeader("To", sendEmailDto.ToEmail)
-	m.SetHeader("Subject", sendEmailDto.Subject)
+	m.SetHeaders(map[string][]string{
+		"From":    {m.FormatAddress(systemVo.EmailUsername, sendEmailDto.FromEmailName)},
+		"To":      sendEmailDto.ToEmails,
+		"Subject": {sendEmailDto.Subject},
+	})
 	m.SetBody("text/html", sendEmailDto.Content)
 	// 附件选项
 	// m.Attach("/home/Alex/lolcat.jpg")
@@ -58,5 +67,6 @@ func SendEmail(sendEmailDto *dto.SendEmailDto) error {
 	ch <- m
 	// 关闭channel来停止守护进程
 	defer close(ch)
+
 	return nil
 }
