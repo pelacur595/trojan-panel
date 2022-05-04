e6bba8c1124912188ef2fb5954ba5af34585347e
jonssonyan <yz808@outlook.com>
2022-05-04 18:51:09 +0800

    fix

--- dao/users.go
+++ dao/users.go
@@ -376,14 +376,12 @@ func SelectEncryPasswordByUsername(username *string) (string, error) {
 
 // 查询禁用或者过期的用户名
 func SelectUsernameByDeletedOrExpireTime() ([]string, error) {
-	var usernames []string
 	buildSelect, values, err := builder.NamedQuery("select username from users where (deleted = {{deleted}} or expire_time <= {{expire_time}}) and password != ''",
 		map[string]interface{}{"deleted": 1, "expire_time": util.NowMilli()})
 	if err != nil {
 		logrus.Errorln(err.Error())
 		return nil, errors.New(constant.SysError)
 	}
-
 	rows, err := db.Query(buildSelect, values...)
 	if err != nil {
 		logrus.Errorln(err.Error())
@@ -391,14 +389,10 @@ func SelectUsernameByDeletedOrExpireTime() ([]string, error) {
 	}
 	defer rows.Close()
 
-	scanMap, err := scanner.ScanMap(rows)
-	if err != nil {
+	var usernames []string
+	if err = scanner.Scan(rows, &usernames); err != nil {
 		logrus.Errorln(err.Error())
 		return nil, errors.New(constant.SysError)
 	}
-	for _, record := range scanMap {
-		usernames = append(usernames, fmt.Sprintf("%s", record["username"]))
-	}
-
 	return usernames, nil
 }
