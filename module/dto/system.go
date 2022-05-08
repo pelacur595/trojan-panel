67e97e924a5d9a0a4a9e495bb29acdaf28aa60b8
jonssonyan <yz808@outlook.com>
2022-05-09 01:03:24 +0800

    fix: email

--- module/dto/system.go
+++ module/dto/system.go
@@ -5,9 +5,9 @@ type SystemUpdateDto struct {
 	OpenRegister       *uint   `json:"openRegister" form:"openRegister" validate:"required,oneof=0 1"` // 是否开放注册
 	RegisterQuota      *int    `json:"registerQuota" validate:"-"`                                     // 注册用户默认配额 单位/byte
 	RegisterExpireDays *uint   `json:"registerExpireDays" validate:"omitempty,gte=0"`                  // 注册用户过期天数 单位/天
-	ExpireWarnEnable   uint    `json:"expireWarnEnable" redis:"expireWarnEnable" validate:"required,oneof=0 1"`
-	ExpireWarnDay      uint    `json:"expireWarnDay" redis:"expireWarnDay" validate:"omitempty,gte=0"`
-	EmailEnable        uint    `json:"emailEnable" redis:"emailEnable" validate:"required,oneof=0 1"`
+	ExpireWarnEnable   *uint   `json:"expireWarnEnable" redis:"expireWarnEnable" validate:"required,oneof=0 1"`
+	ExpireWarnDay      *uint   `json:"expireWarnDay" redis:"expireWarnDay" validate:"omitempty,gte=0"`
+	EmailEnable        *uint   `json:"emailEnable" redis:"emailEnable" validate:"required,oneof=0 1"`
 	EmailHost          *string `json:"emailHost" form:"emailHost" validate:"required,hostname|fqdn,min=3,max=64"`
 	EmailPort          *uint   `json:"emailPort" form:"emailPort" validate:"required,gt=0,lte=65535"`
 	EmailUsername      *string `json:"emailUsername" form:"emailUsername" validate:"required,min=3,max=32"`
