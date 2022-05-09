20c1adf25c8b98f3532185a874ca594a98b38604
jonssonyan <yz808@outlook.com>
2022-05-09 22:52:27 +0800

    fix: sql

--- resource/sql/trojan_panel_db.sql
+++ resource/sql/trojan_panel_db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.35, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: trojan_panel_db
 -- ------------------------------------------------------
--- Server version	5.7.29
+-- Server version	5.7.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -65,7 +65,7 @@ CREATE TABLE `casbin_rule` (
 
 LOCK TABLES `casbin_rule` WRITE;
 /*!40000 ALTER TABLE `casbin_rule` DISABLE KEYS */;
-INSERT INTO `casbin_rule` VALUES ('p','sysadmin','/api/users/selectUserById','GET','','',''),('p','sysadmin','/api/users/createUser','POST','','',''),('p','sysadmin','/api/users/getUserInfo','GET','','',''),('p','sysadmin','/api/users/selectUserPage','GET','','',''),('p','sysadmin','/api/users/deleteUserById','POST','','',''),('p','sysadmin','/api/users/updateUserPassByUsername','POST','','',''),('p','sysadmin','/api/users/updateUserById','POST','','',''),('p','sysadmin','/api/role/selectRoleList','GET','','',''),('p','sysadmin','/api/node/selectNodeById','GET','','',''),('p','sysadmin','/api/node/createNode','POST','','',''),('p','sysadmin','/api/node/selectNodePage','GET','','',''),('p','sysadmin','/api/node/deleteNodeById','POST','','',''),('p','sysadmin','/api/node/updateNodeById','POST','','',''),('p','sysadmin','/api/node/nodeQRCode','GET','','',''),('p','sysadmin','/api/node/nodeURL','GET','','',''),('p','sysadmin','/api/nodeType/selectNodeTypeList','GET','','',''),('p','sysadmin','/api/trojan-gfw/status','GET','','',''),('p','sysadmin','/api/trojan-gfw/restart','POST','','',''),('p','sysadmin','/api/trojan-gfw/stop','POST','','',''),('p','sysadmin','/api/trojan-go/status','GET','','',''),('p','sysadmin','/api/trojan-go/restart','POST','','',''),('p','sysadmin','/api/trojan-go/stop','POST','','',''),('p','sysadmin','/api/dashboard/panelGroup','GET','','',''),('p','sysadmin','/api/system/selectSystemByName','GET','','',''),('p','sysadmin','/api/system/updateSystemById','POST','','',''),('p','sysadmin','/api/system/uploadWebFile','POST','','',''),('p','user','/api/users/getUserInfo','GET','','',''),('p','user','/api/users/updateUserPassByUsername','POST','','',''),('p','user','/api/node/selectNodePage','GET','','',''),('p','user','/api/node/nodeQRCode','GET','','',''),('p','user','/api/node/nodeURL','GET','','',''),('p','user','/api/nodeType/selectNodeTypeList','GET','','',''),('p','user','/api/dashboard/panelGroup','GET','','','');
+INSERT INTO `casbin_rule` VALUES ('p','sysadmin','/api/users/selectUserById','GET','','',''),('p','sysadmin','/api/users/createUser','POST','','',''),('p','sysadmin','/api/users/getUserInfo','GET','','',''),('p','sysadmin','/api/users/selectUserPage','GET','','',''),('p','sysadmin','/api/users/deleteUserById','POST','','',''),('p','sysadmin','/api/users/updateUserPassById','POST','','',''),('p','sysadmin','/api/users/updateUserById','POST','','',''),('p','sysadmin','/api/users/logout','POST','','',''),('p','sysadmin','/api/role/selectRoleList','GET','','',''),('p','sysadmin','/api/node/selectNodeById','GET','','',''),('p','sysadmin','/api/node/createNode','POST','','',''),('p','sysadmin','/api/node/selectNodePage','GET','','',''),('p','sysadmin','/api/node/deleteNodeById','POST','','',''),('p','sysadmin','/api/node/updateNodeById','POST','','',''),('p','sysadmin','/api/node/nodeQRCode','GET','','',''),('p','sysadmin','/api/node/nodeURL','GET','','',''),('p','sysadmin','/api/nodeType/selectNodeTypeList','GET','','',''),('p','sysadmin','/api/dashboard/panelGroup','GET','','',''),('p','sysadmin','/api/dashboard/trafficRank','GET','','',''),('p','sysadmin','/api/system/selectSystemByName','GET','','',''),('p','sysadmin','/api/system/updateSystemById','POST','','',''),('p','sysadmin','/api/system/uploadWebFile','POST','','',''),('p','sysadmin','/api/blackList/selectBlackListPage','GET','','',''),('p','sysadmin','/api/blackList/deleteBlackListByIp','POST','','',''),('p','sysadmin','/api/blackList/createBlackList','POST','','',''),('p','sysadmin','/api/emailRecord/selectEmailRecordPage','GET','','',''),('p','user','/api/users/getUserInfo','GET','','',''),('p','user','/api/users/updateUserPassById','POST','','',''),('p','user','/api/users/logout','POST','','',''),('p','user','/api/node/selectNodePage','GET','','',''),('p','user','/api/node/nodeQRCode','GET','','',''),('p','user','/api/node/nodeURL','GET','','',''),('p','user','/api/nodeType/selectNodeTypeList','GET','','',''),('p','user','/api/dashboard/panelGroup','GET','','',''),('p','user','/api/dashboard/trafficRank','GET','','','');
 /*!40000 ALTER TABLE `casbin_rule` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,39 +95,6 @@ CREATE TABLE `email_record` (
 LOCK TABLES `email_record` WRITE;
 /*!40000 ALTER TABLE `email_record` DISABLE KEYS */;
 /*!40000 ALTER TABLE `email_record` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `menu_list`
---
-
-DROP TABLE IF EXISTS `menu_list`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `menu_list` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-  `name` varchar(10) NOT NULL DEFAULT '' COMMENT '名称',
-  `icon` varchar(20) NOT NULL DEFAULT '' COMMENT '图标',
-  `route` varchar(50) NOT NULL DEFAULT '' COMMENT '路由',
-  `order` bigint(20) unsigned NOT NULL DEFAULT '100' COMMENT '排序 越小越靠前',
-  `parent_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '父级id',
-  `path` varchar(100) NOT NULL DEFAULT '' COMMENT '路径',
-  `level` int(11) unsigned NOT NULL DEFAULT '1' COMMENT '等级',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  KEY `menu_list_name_index` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COMMENT='菜单';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `menu_list`
---
-
-LOCK TABLES `menu_list` WRITE;
-/*!40000 ALTER TABLE `menu_list` DISABLE KEYS */;
-INSERT INTO `menu_list` VALUES (1,'仪表板','dashboard','/dashboard',100,0,'',1,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(2,'用户管理','user','/users-manage',100,0,'',1,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(3,'节点管理','node','/node-manage',100,0,'',1,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(4,'用户列表','','/users-manage/user-list',100,2,'2-',2,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(5,'节点列表','','/node-manage/node-list',100,3,'3-',2,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(6,'系统设置','system','/system',100,0,'',1,'2022-04-01 00:00:00','2022-04-01 00:00:00');
-/*!40000 ALTER TABLE `menu_list` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -222,33 +189,6 @@ INSERT INTO `role` VALUES (1,'sysadmin','系统管理员',0,'',1,'2022-04-01 00:
 UNLOCK TABLES;
 
 --
--- Table structure for table `role_menu_list`
---
-
-DROP TABLE IF EXISTS `role_menu_list`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role_menu_list` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-  `role_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '角色id',
-  `menu_list_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '菜单id',
-  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COMMENT='角色和菜单关系';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `role_menu_list`
---
-
-LOCK TABLES `role_menu_list` WRITE;
-/*!40000 ALTER TABLE `role_menu_list` DISABLE KEYS */;
-INSERT INTO `role_menu_list` VALUES (1,1,1,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(2,1,2,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(3,1,3,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(4,1,4,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(5,1,5,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(6,1,6,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(7,3,1,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(8,3,3,'2022-04-01 00:00:00','2022-04-01 00:00:00'),(9,3,5,'2022-04-01 00:00:00','2022-04-01 00:00:00');
-/*!40000 ALTER TABLE `role_menu_list` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `system`
 --
 
@@ -271,7 +211,7 @@ CREATE TABLE `system` (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COMMENT='系统设置';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COMMENT='系统设置';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -280,7 +220,7 @@ CREATE TABLE `system` (
 
 LOCK TABLES `system` WRITE;
 /*!40000 ALTER TABLE `system` DISABLE KEYS */;
-INSERT INTO `system` VALUES (2,'trojan-panel',1,0,0,0,0,0,'',0,'','','2022-04-01 00:00:00','2022-05-04 23:50:25');
+INSERT INTO `system` VALUES (1,'trojan-panel',1,0,0,0,0,0,'',0,'','','2022-04-01 00:00:00','2022-04-01 00:00:00');
 /*!40000 ALTER TABLE `system` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -332,4 +272,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-07 17:13:29
+-- Dump completed on 2022-05-09 22:51:58
