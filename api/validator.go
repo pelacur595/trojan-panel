27d908dcb7470ae2d6cffb604b3cfc0b5e39ec4d
jonssonyan <yz808@outlook.com>
2022-05-13 01:09:59 +0800

    fix: field validate

--- api/validator.go
+++ api/validator.go
@@ -20,32 +20,32 @@ func InitValidator() {
 
 // 字符串必须是字母和数字的组合
 func validateStr(f validator.FieldLevel) bool {
-	str := f.Field().String()
+	field := f.Field().String()
 	reg := "^[A-Za-z0-9]+$"
 	compile := regexp.MustCompile(reg)
-	return compile.MatchString(str)
+	return field == "" || compile.MatchString(field)
 }
 
 // 邮箱 只支持163 126 qq gmail
 func validateEmail(f validator.FieldLevel) bool {
-	str := f.Field().String()
+	field := f.Field().String()
 	reg := "^([A-Za-z0-9_\\-\\.])+\\@(163.com|126.com|qq.com|gmail.com)$"
 	compile := regexp.MustCompile(reg)
-	return compile.MatchString(str)
+	return field == "" || compile.MatchString(field)
 }
 
 // 正整数
 func validatePositiveInt(f validator.FieldLevel) bool {
-	str := strconv.FormatUint(f.Field().Uint(), 10)
+	field := strconv.FormatUint(f.Field().Uint(), 10)
 	reg := "^[1-9]\\d*$"
 	compile := regexp.MustCompile(reg)
-	return compile.MatchString(str)
+	return compile.MatchString(field)
 }
 
 // 端口
 func validatePort(f validator.FieldLevel) bool {
-	str := strconv.FormatUint(f.Field().Uint(), 10)
+	field := strconv.FormatUint(f.Field().Uint(), 10)
 	reg := "^([0-9]|[1-9]\\d{1,3}|[1-5]\\d{4}|6[0-4]\\d{4}|65[0-4]\\d{2}|655[0-2]\\d|6553[0-5])$"
 	compile := regexp.MustCompile(reg)
-	return compile.MatchString(str)
+	return compile.MatchString(field)
 }
