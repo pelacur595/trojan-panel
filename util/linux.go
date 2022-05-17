2307c7eed26afc16187e973b8c39cff337b2ab9b
jonssonyan <yz808@outlook.com>
2022-05-17 23:11:17 +0800

    fix: ping

--- util/linux.go
+++ util/linux.go
@@ -1,22 +1,25 @@
 package util
 
 import (
+	"errors"
 	"github.com/go-ping/ping"
 	"runtime"
+	"trojan/module/constant"
 )
 
 func Ping(ip string) (int, error) {
 	pingEr, err := ping.NewPinger(ip)
 	if err != nil {
-		panic(err)
+		return -1, errors.New(constant.SysError)
 	}
-	pingEr.Count = 1
+	pingEr.Count = 3
+	pingEr.Timeout = 3
 	if runtime.GOOS == "windows" {
 		pingEr.SetPrivileged(true)
 	}
 	err = pingEr.Run()
 	if err != nil {
-		panic(err)
+		return -1, errors.New(constant.SysError)
 	}
 	milliseconds := pingEr.Statistics().AvgRtt.Milliseconds()
 	return int(milliseconds), nil
