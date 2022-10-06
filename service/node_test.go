efc5dfcfc374efcaf5a429176b8f12a8bf352fe7
jonssonyan <yz808@outlook.com>
2022-10-06 21:49:31 +0800

    fix: api test

--- service/node_test.go
+++ service/node_test.go
@@ -20,7 +20,8 @@ func TestGrpcAddNode(t *testing.T) {
 		TrojanGoSSMethod:        "",
 		TrojanGoSSPassword:      "",
 	}
-	if err := core.AddNode("127.0.0.1", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Vm8iOnsiaWQiOjEsInF1b3RhIjowLCJkb3dubG9hZCI6MCwidXBsb2FkIjowLCJ1c2VybmFtZSI6InN5c2FkbWluIiwiZW1haWwiOiIiLCJyb2xlSWQiOjEsImRlbGV0ZWQiOjAsImV4cGlyZVRpbWUiOjAsImNyZWF0ZVRpbWUiOiIwMDAxLTAxLTAxVDAwOjAwOjAwWiJ9LCJleHAiOjE2NjQwMTQyNjksImlzcyI6InRyb2phbi1wYW5lbCJ9.vUbGGp42XTyndNNH01aSj6YW_bfck-jmzUcs1JtVMb0", &dto); err != nil {
+	if err := core.AddNode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Vm8iOnsiaWQiOjEsInF1b3RhIjowLCJkb3dubG9hZCI6MCwidXBsb2FkIjowLCJ1c2VybmFtZSI6InN5c2FkbWluIiwiZW1haWwiOiIiLCJyb2xlSWQiOjEsImRlbGV0ZWQiOjAsImV4cGlyZVRpbWUiOjAsImNyZWF0ZVRpbWUiOiIwMDAxLTAxLTAxVDAwOjAwOjAwWiJ9LCJleHAiOjE2NjQwMTQyNjksImlzcyI6InRyb2phbi1wYW5lbCJ9.vUbGGp42XTyndNNH01aSj6YW_bfck-jmzUcs1JtVMb0",
+		"127.0.0.1", &dto); err != nil {
 		fmt.Println(err.Error())
 	}
 }
