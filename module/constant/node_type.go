ca65cc3f365f7838f588833849248b984b98d673
jonssonyan <yz808@outlook.com>
2022-11-23 15:34:26 +0800

    fix: node

--- module/constant/node_type.go
+++ module/constant/node_type.go
@@ -1,7 +1,8 @@
 package constant
 
 const (
-	XrayName     string = "xray"      // type 1
-	TrojanGoName string = "trojan-go" // type 2
-	HysteriaName string = "hysteria"  // type 3
+	XrayName       string = "xray"       // type 1
+	TrojanGoName   string = "trojan-go"  // type 2
+	HysteriaName   string = "hysteria"   // type 3
+	NaiveProxyName string = "naiveproxy" // type 4
 )
--- service/node_test.go
+++ service/node_test.go
@@ -8,17 +8,9 @@ import (
 
 func TestGrpcAddNode(t *testing.T) {
 	dto := core.NodeAddDto{
-		NodeTypeId:              2,
-		Port:                    443,
-		Ip:                      "127.0.0.1",
-		TrojanGoSni:             "",
-		TrojanGoMuxEnable:       0,
-		TrojanGoWebsocketEnable: 0,
-		TrojanGoWebsocketPath:   "",
-		TrojanGoWebsocketHost:   "",
-		TrojanGoSSEnable:        0,
-		TrojanGoSSMethod:        "",
-		TrojanGoSSPassword:      "",
+		NodeTypeId: 4,
+		Port:       4444,
+		Ip:         "demo.wellveryfunny.xyz",
 	}
 	if err := core.AddNode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Vm8iOnsiaWQiOjEsInF1b3RhIjowLCJkb3dubG9hZCI6MCwidXBsb2FkIjowLCJ1c2VybmFtZSI6InN5c2FkbWluIiwiZW1haWwiOiIiLCJyb2xlSWQiOjEsImRlbGV0ZWQiOjAsImV4cGlyZVRpbWUiOjAsImNyZWF0ZVRpbWUiOiIwMDAxLTAxLTAxVDAwOjAwOjAwWiJ9LCJleHAiOjE2NjQwMTQyNjksImlzcyI6InRyb2phbi1wYW5lbCJ9.vUbGGp42XTyndNNH01aSj6YW_bfck-jmzUcs1JtVMb0",
 		"127.0.0.1", &dto); err != nil {
