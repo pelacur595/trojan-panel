c209b8d16a1e6b65ad746e2a99a1ba4026382c7d
jonssonyan <yz808@outlook.com>
2023-01-17 13:44:34 +0800

    fix: test

--- service/node_test.go
+++ service/node_test.go
@@ -10,7 +10,7 @@ func TestGrpcAddNode(t *testing.T) {
 	dto := core.NodeAddDto{
 		NodeTypeId: 4,
 		Port:       883,
-		Ip:         "demo.wellveryfunny.xyz",
+		Domain:     "demo.wellveryfunny.xyz",
 	}
 	if err := core.AddNode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Vm8iOnsiaWQiOjEsInF1b3RhIjowLCJkb3dubG9hZCI6MCwidXBsb2FkIjowLCJ1c2VybmFtZSI6InN5c2FkbWluIiwiZW1haWwiOiIiLCJyb2xlSWQiOjEsImRlbGV0ZWQiOjAsImV4cGlyZVRpbWUiOjAsImNyZWF0ZVRpbWUiOiIwMDAxLTAxLTAxVDAwOjAwOjAwWiIsInJvbGVzIjpbInN5c2FkbWluIiwiYWRtaW4iLCJ1c2VyIl19LCJleHAiOjE2NjkxOTgxMDAsImlzcyI6InRyb2phbi1wYW5lbCJ9.9DYvvo0F-XS55WUy5J8Lmj8gim243yDd_BiZZnNv0is",
 		"127.0.0.1", &dto); err != nil {
