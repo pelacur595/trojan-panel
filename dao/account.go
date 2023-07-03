dd907acc2b654b46cce72acdd58acd09068762aa
jonssonyan <yz808@outlook.com>
2023-07-03 15:39:47 +0800

    fix: v2.1.5 bug

--- dao/account.go
+++ dao/account.go
@@ -429,7 +429,7 @@ func UpdateAccountQuotaOrDownloadOrUploadOrDeletedByUsernames(usernames []string
 
 // SelectAccountUsernameByDeletedOrExpireTime 查询禁用或者过期的用户名
 func SelectAccountUsernameByDeletedOrExpireTime() ([]string, error) {
-	buildSelect, values, err := builder.NamedQuery("select username from account where quota != 0 and (deleted = 1 or expire_time <= unix_timestamp(NOW()) * 1000",
+	buildSelect, values, err := builder.NamedQuery("select username from account where quota != 0 and (deleted = 1 or expire_time <= unix_timestamp(NOW()) * 1000)",
 		nil)
 	if err != nil {
 		logrus.Errorln(err.Error())
