24a06cc33787df0dffdb3faf6c1fd6bcc6dbbec6
jonssonyan <yz808@outlook.com>
2023-12-15 10:16:39 +0800

    fix: version

--- model/constant/system.go
+++ model/constant/system.go
@@ -33,5 +33,5 @@ const (
 	TrojanPanelCrtFile      string = "trojan-panel.crt"
 	TrojanPanelKeyFile      string = "trojan-panel.key"
 
-	TrojanPanelVersion = "v2.3.0"
+	TrojanPanelVersion = "v2.3.1"
 )
